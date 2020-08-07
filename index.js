ca48fd779cb666791d2d70563cf81264b1914263
Tim Ellison <timothyellison85@gmail.com>
2020-08-06 20:19:34 -0700

    connect to Apollo Studio

--- index.js
+++ index.js
@@ -39,7 +39,13 @@ const resolvers = {
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  engine: {
+    reportSchema: true,
+  },
+});
 
 // The `listen` method launches a web server.
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
